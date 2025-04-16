e97efeccdd7e3736ebb5cb01b44e301442275908
joanadubey <161479715+joanadubey@users.noreply.github.com>
2025-04-16 10:01:14 +0200

    animations

--- src/js/main.js
+++ src/js/main.js
@@ -13,18 +13,57 @@ gsap.to(".visuel-homepage", {
     markers: true,
   },
 });
+
 var branches = document.querySelectorAll(".branches");
 
 branches.forEach((branche) => {
   gsap.to(branche, {
     scrollTrigger: {
+      pin: branche,
       trigger: branche,
-      start: "bottom bottom",
-      scrub: 1,
+      start: "top",
+      end: "bottom",
+      pinSpacing: false,
       markers: true,
     },
-    x: 400,
-    rotation: 90,
-    duration: 5,
   });
 });
+
+var projects = document.querySelectorAll(".projets");
+
+projects.forEach((project) => {
+  gsap.to(project, {
+    scrollTrigger: {
+      pin: project,
+      trigger: project,
+      start: "top",
+      end: "bottom",
+      pinSpacing: false,
+      markers: true,
+    },
+  });
+});
+
+// gsap.to(".projets", {
+//   scrollTrigger: {
+//     pin: ".projets",
+//     trigger: ".projets",
+//     start: "top",
+//     end: "bottom",
+//     pinSpacing: false,
+//     markers: true,
+//   },
+// });
+// var branches = document.querySelectorAll(".projets");
+
+// branches.forEach((branche) => {
+//   gsap.to(branche, {
+//     scrollTrigger: {
+//       trigger: branche,
+//       start: "bottom bottom",
+//       scrub: 1,
+//       markers: true,
+//     },
+//     duration: 5,
+//   });
+// });
