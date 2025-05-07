7bb7fdd2235d087cd6eefec0a26aa49763b7d250
joanadubey <161479715+joanadubey@users.noreply.github.com>
2025-05-07 12:19:17 +0200

    rendu

--- src/js/_custom-cursor.js
+++ src/js/_custom-cursor.js
@@ -1,3 +1,42 @@
+// const cursor = document.querySelector(".custom-cursor");
+// let mouseX = 0,
+//   mouseY = 0;
+// let currentX = 0,
+//   currentY = 0;
+
+// function animateCursor() {
+//   currentX += (mouseX - currentX) * 0.03;
+//   currentY += (mouseY - currentY) * 0.03;
+//   cursor.style.transform = `translate(${currentX - 60}px, ${currentY - 60}px)`;
+//   requestAnimationFrame(animateCursor);
+// }
+// animateCursor();
+
+// document.addEventListener("mousemove", (e) => {
+//   mouseX = e.clientX;
+//   mouseY = e.clientY;
+// });
+
+// const projets = document.querySelectorAll(".projets");
+// projets.forEach((projet) => {
+//   projet.addEventListener("mouseenter", () => {
+//     cursor.style.opacity = 1;
+//   });
+//   projet.addEventListener("mouseleave", () => {
+//     cursor.style.opacity = 0;
+//   });
+// });
+
+// const nextElements = document.querySelectorAll(".next");
+// nextElements.forEach((nextEl) => {
+//   nextEl.addEventListener("mouseenter", () => {
+//     cursor.style.opacity = 1;
+//   });
+//   nextEl.addEventListener("mouseleave", () => {
+//     cursor.style.opacity = 0;
+//   });
+// });
+
 const cursor = document.querySelector(".custom-cursor");
 let mouseX = 0,
   mouseY = 0;
@@ -7,7 +46,7 @@ let currentX = 0,
 function animateCursor() {
   currentX += (mouseX - currentX) * 0.03;
   currentY += (mouseY - currentY) * 0.03;
-  cursor.style.transform = `translate(${currentX - 60}px, ${currentY - 60}px)`;
+  cursor.style.transform = `translate(${currentX - 60}px, ${currentY - 60}px)`; // Ajuste la position du curseur
   requestAnimationFrame(animateCursor);
 }
 animateCursor();
